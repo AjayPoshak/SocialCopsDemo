301d86acf07fb7b4b530ea775efb7a474fe819c9
Ajay Poshak <poshakajay@gmail.com>
2016-09-25 17:20:36 +0530

    Improvements done as suggested by jshint

--- js/Controllers/battingStatsController.js
+++ js/Controllers/battingStatsController.js
@@ -3,32 +3,35 @@
  * This file is the controller of battingStats page.
  */
 angular.module('socialCopsDemo')
-  .controller('battingStatsController', battingStatsController);
+    .controller('battingStatsController', battingStatsController);
 
-  function battingStatsController($scope, $rootScope){
+//Injecting the dependencies explicitly to avoid post minification errors
+battingStatsController.$inject = ['$scope', '$rootScope'];
+
+function battingStatsController($scope, $rootScope) {
     let data = {};
-    if(localStorage.getItem('data') != undefined || localStorage.getItem('data') != ''){
-      data  = JSON.parse(localStorage.getItem("data"));
+    if (localStorage.getItem('data') !== undefined || localStorage.getItem('data') !== '') {
+        data = JSON.parse(localStorage.getItem("data"));
     }
-    let runsByYear = new Array();
+    console.log("This is a demo");
+    let runsByYear = [];
     let runsTossLost = 0;
     let runsTossWon = 0;
-    for(let i = 0; i < data.length-1; i++){
-      let responseIndex = checkYear(data[i].date.slice(-4));
-      if(responseIndex == -1){
-        let obj = new Object();
-        obj.year = parseInt(data[i].date.slice(-4));
-        obj.runs = parseInt(data[i].batting_score);
-        runsByYear.push(obj);
-      }
-      else {
-        if(!$rootScope.ifNaN(data[i].batting_score))
-          runsByYear[responseIndex].runs += parseInt(data[i].batting_score);
-      }
-      if(!$rootScope.ifNaN(data[i].batting_score) && data[i].toss == "lost")
-        runsTossLost += parseInt(data[i].batting_score);
-      else if(!$rootScope.ifNaN(data[i].batting_score) && data[i].toss == "won")
-        runsTossWon += parseInt(data[i].batting_score);
+    for (let i = 0; i < data.length - 1; i++) {
+        let responseIndex = checkYear(data[i].date.slice(-4));
+        if (responseIndex == -1) {
+            let obj = {};
+            obj.year = parseInt(data[i].date.slice(-4));
+            obj.runs = parseInt(data[i].batting_score);
+            runsByYear.push(obj);
+        } else {
+            if (!$rootScope.ifNaN(data[i].batting_score))
+                runsByYear[responseIndex].runs += parseInt(data[i].batting_score);
+        }
+        if (!$rootScope.ifNaN(data[i].batting_score) && data[i].toss == "lost")
+            runsTossLost += parseInt(data[i].batting_score);
+        else if (!$rootScope.ifNaN(data[i].batting_score) && data[i].toss == "won")
+            runsTossWon += parseInt(data[i].batting_score);
     }
     /**
      * This function checks whether that year has already been inserted into
@@ -37,58 +40,55 @@ angular.module('socialCopsDemo')
      * @return {Number}       -1, if year is not in array; location, if year is
      *                              already in array.
      */
-    function checkYear(param){
-      let loc = -1;
-      for(let itr = 0; itr < runsByYear.length; itr++){
-        if(runsByYear[itr].year == param){
-          loc = itr;
-          break;
+    function checkYear(param) {
+        let loc = -1;
+        for (let itr = 0; itr < runsByYear.length; itr++) {
+            if (runsByYear[itr].year == param) {
+                loc = itr;
+                break;
+            }
         }
-      }
-      return loc;
-    };
-    let runsData = new Array();
-    let runsYear = new Array();
-    for(let i = 0; i < runsByYear.length; i++){
-      runsData.push(runsByYear[i].runs);
-      runsYear.push(runsByYear[i].year);
-    };
+        return loc;
+    }
+    let runsData = [],
+        runsYear = [];
+    for (let i = 0; i < runsByYear.length; i++) {
+        runsData.push(runsByYear[i].runs);
+        runsYear.push(runsByYear[i].year);
+    }
     let runByYearGraph = document.getElementById('runGraphByYear');
     let runsByToss = document.getElementById('runsByToss').getContext("2d");
     let lineChartData = {
-      labels: runsYear,
-      datasets: [
-          {
-              label: "My First dataset",
-              fill: false,
-              lineTension: 0.1,
-              backgroundColor: "rgba(75,192,192,0.4)",
-              borderColor: "rgba(75,192,192,1)",
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: "rgba(75,192,192,1)",
-              pointBackgroundColor: "#fff",
-              pointBorderWidth: 1,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: "rgba(75,192,192,1)",
-              pointHoverBorderColor: "rgba(220,220,220,1)",
-              pointHoverBorderWidth: 2,
-              pointRadius: 1,
-              pointHitRadius: 10,
-              data: runsData,
-              spanGaps: false,
-          }
-      ]
+        labels: runsYear,
+        datasets: [{
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: runsData,
+            spanGaps: false,
+        }]
     };
     let runsByTossChartData = {
-      labels: [
-        "Runs Scored while toss was lost",
-        "Runs Scored while toss was won"
-    ],
-    datasets: [
-        {
+        labels: [
+            "Runs Scored while toss was lost",
+            "Runs Scored while toss was won"
+        ],
+        datasets: [{
             data: [runsTossLost, runsTossWon],
             backgroundColor: [
                 "#FF6384",
@@ -101,11 +101,11 @@ angular.module('socialCopsDemo')
         }]
     };
     let runLineChart = new Chart(runByYearGraph, {
-      type: 'line',
-      data: lineChartData
+        type: 'line',
+        data: lineChartData
     });
     let runsByTossPieChart = new Chart(runsByToss, {
-      type: 'pie',
-      data: runsByTossChartData
+        type: 'pie',
+        data: runsByTossChartData
     });
-  };
+}
